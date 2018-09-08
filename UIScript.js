5d5cd3e2d5f074e44a699eaa8c96ca7c9734f2c2
Velkaus <kylehosford@gmail.com>
2018-09-07 23:15:00 -0400

    Sprint 1 - searching
    added searching capacity
    - displays parsed data in an unordered list
    - close search button exists, but is dysfunct for now
    - I know at least 1 person reads these

--- UIScript.js
+++ UIScript.js
@@ -1,39 +1,72 @@
-// window.onloadend = setup();
-// document.addEventListener("DOMContentLoaded", setup, false);
+// window.onloadend = main();
+document.addEventListener("DOMContentLoaded", main, false);
 // document.addEventListener("resize", setup, false);
 
+var cheatJSON = 
+{"Jules Verne":["Igor Sikorsky","Konstantin Tsiolkovsky","Simon Lake","Yuri Gregarin","Wernher von Braun"],"Isaac Asimov":["Elon Musk"],"Ray Bradbury":["Bill Nye","Steve Jobs","Hermann Oberth"],"Isaac Newton":["Isaac Asimov","Ray Bradbury"]};
 
-function setup() {
+
+
+function main() {
+
+	// document.getElementById("searchButton").addEventListener("click", pullData());
 	
-	//make elements look pretty in the window
-
-	//Title block
-	var titleBlock = document.getElementById("Welcome-Message");
-	var titleStyle = window.getComputedStyle(titleBlock);
-
-	console.log(titleStyle.getPropertyValue('left').slice(0, -2));
-	console.log(titleStyle.getPropertyValue('width'));
-
-	var titleLeft = titleStyle.getPropertyValue('left');
-	var titleWidth = titleStyle.getPropertyValue('width');
-
-	titleLeft = parseInt( titleLeft.slice(0,-2), 10);
-	titleWidth = parseInt( titleWidth.slice(0,-2), 10);
-	titleBlock.style.left = titleLeft - titleWidth/2;
-
-	//search bar
-	var searchBar = document.getElementById("Home-Search-Bar");
-	var searchStyle = window.getComputedStyle(searchBar);
-
-	console.log(searchStyle.getPropertyValue('left').slice(0, -2));
-	console.log(searchStyle.getPropertyValue('width'));
-
-	var searchLeft = searchStyle.getPropertyValue('left');
-	var searchWidth = searchStyle.getPropertyValue('width');
-
-	searchLeft = parseInt( searchLeft.slice(0,-2), 10);
-	searchWidth = parseInt( searchWidth.slice(0,-2), 10);
-	searchBar.style.left = searchLeft - searchWidth/2;
-
-	//adjust searchbar height to accomodate button
 }
+
+function pullData(){
+	var searchTerm = document.getElementById("searchBox").value;
+	console.log(searchTerm);
+
+	document.getElementById("closeButton").style.visibility = "visible";
+	//clear prev search
+
+	var results = document.getElementById("results")
+	results.innerHTML = ""; 
+
+	//check for bad query
+	if( cheatJSON[ searchTerm ] != undefined ) {
+		//good query, build results
+		var lookup = cheatJSON[ searchTerm ];
+		results.innerHTML = "Found : <b>" + searchTerm + "</b> with " + lookup.length + " influences. <br>Known influences :<br><ul> ";
+		for(key in lookup ) {
+			results.innerHTML += "<li> " + lookup[ key ] + " </li>";
+		}
+		results.innerHTML += "</ul>";
+	}
+
+
+}
+
+function closeSearch() {
+	document.getElementById("closeButton").style.visibility = "hidden";
+	//clear prev search
+
+	var results = document.getElementById("results")
+	results.innerHTML = ""; 
+}
+
+
+function loadJson(){
+	fetch("https://raw.githubusercontent.com/Masbuc53/JIC8202/master/FakeData.json").then( response =>{
+		console.log(response.json().name);
+		return response.json();
+	}).catch(err => {
+		console.log(err);
+	})
+}
+
+
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
