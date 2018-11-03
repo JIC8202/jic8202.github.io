fef2d3f48c60c5995c1518d4012e51bf780a957c
despair <>
2018-11-03 02:57:29 -0400

    Color nodes based on group
    Revert [testing] Load data from backend server

--- src/legend.js
+++ src/legend.js
@@ -1,10 +1,24 @@
 import * as d3 from "d3";
 
-export function createNodeColorLegend(colorScale) {
-    const palette = d3.select("#legend-palette");
-    var colorScale = d3.range(5).map(t => colorScale(t / 4)).reverse();
-    var swatch = palette.selectAll('div').data(colorScale);
-    swatch.enter().append('div')
-        .attr('class', 'legend-swatch')
-        .style('background-color', d => d);
+export function createNodeColorLegend() {
+    const labels = [
+        "SF author",
+        "Scientist",
+        "Both"
+    ];
+    var data = labels.map((e, i) => ({
+        label: e,
+        color: d3.schemeSet1[i]
+    }));
+
+    const legend = d3.select("#legend");
+    const row = legend.selectAll("div")
+        .data(data)
+        .enter().append("div")
+        .attr("class", "legend-row");
+    row.append("div")
+        .attr("class", "legend-swatch")
+        .style("background-color", d => d.color);
+    row.append("div")
+        .text(d => d.label);
 }
