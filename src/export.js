4baacc6a885fd1d65907029ecdaab412c34f440d
despair <>
2018-11-07 16:09:45 -0500

    Add CSV header row

--- src/export.js
+++ src/export.js
@@ -3,7 +3,7 @@ import {saveAs} from "file-saver";
 
 export function setupExport(id, data) {
     d3.select("#sidebar-export").on("click", function() {
-        var csv = [];
+        var csv = ["Source,Target\n"];
         for (let link of data.links) {
             if (link.source.id == id || link.target.id == id) {
                 csv.push(link.source.id + "," + link.target.id + "\n");
