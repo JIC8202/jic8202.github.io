45ab4329bce8dc32a21af41074965fae9cd1879f
despair <>
2018-11-16 06:03:49 -0500

    Fix sidebar element heights on Chrome

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,30 +2,23 @@ import * as d3 from "d3";
 
 export function showSidebar(name, influencers, influencees, network) {
     d3.select("#detail").classed("active", true);
-    d3.select('#detail-name').text(name);
+    d3.select("#detail-name").text(name);
 
-    updateJoin('#detail-linksTo', influencers, network);
-    updateJoin('#detail-linksFrom', influencees, network);
+    updateJoin("#detail-linksTo", influencers, network);
+    updateJoin("#detail-linksFrom", influencees, network);
 
     // detect overflow (scrollbar) on .detail-links
-    // if there is overflow, set flex-basis: 100%;
+    // if there is overflow, set a constant flex-basis
     // so that the two lists take up equal space
 
     // this is done to avoid uneven size of lists
     // if their content sizes are disproportionate
     // i.e. a node having many more links in one category
-
-    // flex-basis: 100%; should not be applied when
-    // there is no overflow, as it would create
-    // unnecessary scrollbars when the content already
-    // fits on the page
-    d3.selectAll('.detail-links').style('flex-basis', null);
-    let overflow = d3.selectAll('.detail-links').filter(function() {
+    let lists = d3.selectAll(".detail-links");
+    lists.classed("overflow", false); // reset before checking
+    lists.filter(function() {
         return this.scrollHeight > this.clientHeight;
-    });
-    if (!overflow.empty()) {
-        overflow.style('flex-basis', '100%');
-    }
+    }).classed("overflow", true);
 }
 
 function updateJoin(selector, data, network) {
@@ -34,10 +27,10 @@ function updateJoin(selector, data, network) {
         .data(data);
     row.enter().append("li")
         .text(d => d)
-        .on('click', onClick.bind(network))
+        .on("click", onClick.bind(network))
         .merge(row)
         .text(d => d)
-        .on('click', onClick.bind(network));
+        .on("click", onClick.bind(network));
     row.exit().remove();
 }
 
