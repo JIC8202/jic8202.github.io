3909bea7eeafc0aab4006a7bc3e1915ce416d369
despair <>
2018-11-16 23:56:32 -0500

    Use d3-dsv for generating CSV data
    Fixes #30

--- src/export.js
+++ src/export.js
@@ -3,13 +3,15 @@ import {saveAs} from "file-saver";
 
 export function setupExport(id, data) {
     d3.select("#detail-export").on("click", function() {
-        var csv = ["Source,Target\n"];
+        var rows = [["Source", "Target"]];
         for (let link of data.links) {
             if (link.source.id == id || link.target.id == id) {
-                csv.push(link.source.id + "," + link.target.id + "\n");
+                rows.push([link.source.id, link.target.id]);
             }
         }
-        var file = new File(csv, id + ".csv", {type: "text/plain;charset=utf-8"});
-        saveAs(file);
+
+        let csv = d3.csvFormatRows(rows);
+        let blob = new Blob([csv], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, id + ".csv");
     })
 }
